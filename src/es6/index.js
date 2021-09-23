3986c8b33d43df6722bd1c15aaf7879177d88783
felipe <feliperestrepomeneses@gmail.com>
2021-09-22 22:08:50 -0500

    En esta clase vimos sobre promesas, y como se utiliza el resolve, reject, y valores como .then y .catch, se puede profundizar mas en el curso de asincronismo. Tambien vimos las Arrow Functions o funciones tipo lecha y la propiedad de objetos mejorada en ES6

--- src/es6/index.js
+++ src/es6/index.js
@@ -128,3 +128,107 @@ a = "a";
 console.log(a);
 
 //Hasta aqui la segunda clase
+
+
+
+
+
+
+//Propiedad de objetos mejorada
+
+/* Antes teniamos que crear un objeto, definiendo un
+nombre para sus elementos y luego vinculandolos a una
+variable, ahora, con ES6, solo necesitamos llamar el
+nombre de esa variable */
+
+let name = "felipe";
+let age = 23;
+
+//ES5
+obj = { name: name, age: age };
+//ES6
+obj2 = { name, age };
+
+console.log(obj);
+console.log(obj2);
+
+//Si vemos el output, tiene el mismo output
+
+
+
+
+
+
+//Arrow Functions o Funciones tipo flecha
+
+//Brindan una sintaxis mas reducida
+
+//Crearemos una const, que sera un Array donde tendremos dos objetos
+
+const names = [
+    { name: 'Oscar', age: 23 },
+    { name: 'Yesica', age: 27},
+]
+
+//llamaremos esos elementos de forma tradicional
+
+let listOfNames = names.map(function (item) {
+    console.log(item.name);
+})
+
+//Con Arrow Function
+
+let listOfNames2 = names.map(item => console.log(item.name));
+
+// Otra forma en la que veremos Arrow Functions es con constantes
+
+const listOfNames3 = (name, age, country) => {
+    //Bloque de codigo que necesito
+}
+
+// Cuando estamos pasando un solo elemento o parametro
+
+const listOfNames4 = name => {
+    //Bloque de codigo que necesito
+}
+
+/*La ultima es cuando podemos directamente hacer 
+el Arrow Function con un solo valor, sin asignar bloques de llaves, 
+por ejemplo sacandole el cuadrado a un numero */
+
+const square = num => num * num;
+
+
+
+
+
+
+// La Promesa o promise
+
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if (false) {
+            resolve('Hey!');
+        } else {
+            reject('Ups!!');
+        }
+    });
+}
+
+//Para ejecutarlo la siguiente funcion, con los valores .then y .catch
+
+helloPromise()
+.then(response => console.log(response))
+.then(() => console.log('hola'))
+.catch(error => console.log(error));
+
+//Si ejecutamos, solo tenemos que cambiar el false o true de la condicion para cambiar ese output.
+//Hasta aqui la tercera leccion
+
+
+
+
+
+
+
+
