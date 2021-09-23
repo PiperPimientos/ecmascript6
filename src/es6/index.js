8f665cdfe2845b04e3380c73aadf13de338aa2a6
felipe <feliperestrepomeneses@gmail.com>
2021-09-22 23:58:23 -0500

    Hasta aqui vamos con la 4ta leccion, es muy importante dividir y ver estos dos temas importantes, como lo son clases, que se pueden hacer con una sintaxis mas clara y, despues, los Modulos, que tienen una larga explicacion de como separar la logica en otros archivos, exportar esos elementos e importarlos desde el archivo index, ademas agregamos un comentario importante y en aclaracion, para el export default y el export nombrado, que son sustancialmente distintos y la forma de hacerlo con ES5, porque puede que por ejemplo node no lo soporte aun

--- src/es6/index.js
+++ src/es6/index.js
@@ -230,5 +230,90 @@ helloPromise()
 
 
 
+//Clases, sintaxis claras para manejar objetos y herencias
+
+class calculator {
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+/* Recapitulando: tenemos una clase, que tiene un constructor,
+el cual esta asignando dentro del scope global las dos variables
+despues vamos a tener el metodo sum, al cual le vamos a pasar
+los elementos que seran los dos valores a sumar, y estoy
+asignando estas variables que previamente construi dentro de mi clase
+a esos valores que recibo y al final hacemos la suma, retornando este elemento */
+
+const calc = new calculator();
+console.log(calc.sum(2, 2));
 
 
+
+
+//Import, desde el archivo Module.js, donde tenemos una funcion hello que exportamos.
+
+import { hello } from './module';
+
+hello();
+
+/* Veo un pequeño error en la parte de módulos, Óscar exportó el módulo usando un export default y lo importó como un export nombrado.
+También quiero aclarar que si a algunos no les funciona tal vez sea porque es una sintaxis que no está aún soportada por las últimas versiones de Node y para ese caso tocaría usar la sintaxis antigua.
+ES6
+export default
+Con export default podemos exportar una variable, o función por defecto de un archivo así: */
+function hello() {
+	return 'Hello!'
+}
+
+export default hello
+//Para importarla no necesitamos hacerlo con su nombre, podemos usar cualquier nombre y por defecto nos traerá la única variable o función que exportó así:
+import myHello from './module'
+
+console.log(myHello())
+/* export nombrado
+A diferencia del export default en esta ocasión podemos exportar más de una variable o función que se encuentre en nuestro archivo, pero para importarlas debemos hacerlo con el nombre exacto de esa variable entre llaves.
+Para exportar lo hacemos así: */
+export function hello() {
+	return 'Hello!'
+}
+
+export const bye = 'Bye!'
+//Podemos importar solo lo que necesitemos así:
+import { hello } from './module'
+
+console.log(hello())
+//También podemos importar más de un elemento nombrando cada uno
+import { hello, bye } from './module'
+
+console.log(hello())
+console.log(bye)
+//Podemos cambiarles los nombres
+import { hello, bye as byeGreeting } from './module'
+
+console.log(hello())
+console.log(byeGreeting)
+//Y podemos importar todas las funciones de una sola vez
+import * as allGreetings from './module'
+
+console.log(allGreetings.hello())
+console.log(allGreetings.bye)
+/* Anterior sintaxis
+Cuando no nos funcione esta sintaxis porque no la tengamos soportada podemos usar la anterior
+Para exportar lo hacemos así: */
+function hello() {
+	return 'Hello!'
+}
+
+module.exports = hello
+//Para importar así:
+const hello = require('./module')
+
+console.log(hello())
+//Con la sintaxis antigua también podemos exportar más de una variable o función pasándolas como un objeto.
