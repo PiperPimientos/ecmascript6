357d8cd0a710274afb4a442f6cf0542cce91bec2
felipe <feliperestrepomeneses@gmail.com>
2021-09-23 22:33:17 -0500

    Creamos la carpeta es6 dentro de nuestra carpeta src, para crear un index.js donde tenemos las dos principales actualizaciones de ES7, que son el Includes, que nos sirve para validar si un valor esta dentro de un elemento, y luego, las sintaxis de las potencias en JS

---
+++ src/es7/index.js
@@ -0,0 +1,36 @@
+//Veremos el Includes
+
+//Crearemos un array que contenga numeros hata el 7
+
+let numbers = [1, 2, 3, 7, 8]
+
+// ahora, para utilizar el includes, tenemos que validar con un if y la siguiente sintaxis
+//Validaremos que el array contenga el 7
+if (numbers.includes(7)) {
+    console.log('Si se encuentra el valor 7');
+} else {
+    console.log ('No se encuentra')
+}
+
+//Si le damos Run Code, veremos que si se encuentra el valor 7, pero podemos intentarlo con otro valor.
+//Anteriormente utilizabamos indexof y un codigo mas complejo
+
+
+
+
+
+
+
+//Como podemos elevar a la potencia con JS
+//Tenemos una estructura de 3 variables, que una sera el numero base, otra, el exponente y finalmente el resultado
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
+
+// Si le damos run code, sale 64, pero probemos cambiando los valores
+
+
+
